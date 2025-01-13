580eb8da9c28409c626353b791b90ccd817aef10
ManuCarus <manu.carus@mailbox.org>
2025-01-13 12:47:07 +0100

    Add files via upload

---
+++ NegotiateProxy/Properties/AssemblyInfo.cs
@@ -0,0 +1,88 @@
+﻿/*
+   COPYRIGHT AND PERMISSION NOTICE
+
+   NegotiateProxy - Reverse proxy to handle SPNEGO / Kerberos communication locally and
+                    transparently on behalf of client applications that are themselves 
+                    not SPNEGO/Kerberos-aware.
+
+   Copyright (c) 2012 by Manu Carus (mailto:manu.carus@ethical-hacking.de)
+
+   The Initial Developer of the Original Code is Manu Carus.
+   All rights reserved.
+
+   Permission to use, copy, modify, and distribute this software for any
+   purpose, subject to the provisions described below, without fee is
+   hereby granted, provided that this entire notice is included in all
+   copies of any software that is or includes a copy or modification of
+   this software and in all copies of the supporting documentation for
+   such software.
+
+   THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD-PARTY RIGHTS.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+   OTHERWISE, ARISING FROM, OUT OF OR INCONNECTION WITH THE SOFTWARE OR THE
+   USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+   Except as contained in this notice, the name of a copyright holder shall
+   not be used in advertising or otherwise to promote the sale, use or other
+   dealings in this Software without prior written permission of the copyright
+   holder.
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NegotiateProxy")]
+[assembly: AssemblyDescription("Reverse proxy to handle SPNEGO / Kerberos communication locally and transparently on behalf of client applications that are themselves not SPNEGO/Kerberos-aware.")]
+[assembly: AssemblyCompany("Manu Carus")]
+[assembly: AssemblyProduct("NegotiateProxy")]
+[assembly: AssemblyCopyright("Copyright © 2012 Manu Carus (mailto:manu.carus@ethical-hacking.de)")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyTrademark("The Initial Developer of the Original Code is Manu Carus. " +
+                             "All rights reserved. " +
+                             "" +
+                             "Permission to use, copy, modify, and distribute this software for any " +
+                             "purpose, subject to the provisions described below, without fee is " +
+                             "hereby granted, provided that this entire notice is included in all " +
+                             "copies of any software that is or includes a copy or modification of " +
+                             "this software and in all copies of the supporting documentation for " +
+                             "such software. " +
+                             "" +
+                             "THIS SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS " +
+                             "OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
+                             "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD-PARTY RIGHTS. " +
+                             "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, " +
+                             "DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR " +
+                             "OTHERWISE, ARISING FROM, OUT OF OR INCONNECTION WITH THE SOFTWARE OR THE " +
+                             "USE OR OTHER DEALINGS IN THE SOFTWARE. " +
+                             "" +
+                             "Except as contained in this notice, the name of a copyright holder shall " +
+                             "not be used in advertising or otherwise to promote the sale, use or other " +
+                             "dealings in this Software without prior written permission of the copyright " +
+                             "holder.")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37c117df-8861-4a9a-bb98-da6c809b232e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
